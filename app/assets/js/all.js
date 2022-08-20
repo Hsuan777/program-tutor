51a540112003e1e0c111df8f19f822958bed1189
Vic <syuan0212@gmail.com>
2022-08-20 23:28:09 +0800

    add: pay

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,10 @@
 console.log('Hello!');
+import { Datepicker } from 'vanillajs-datepicker/Datepicker';
 
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
-});
+$(function() {
+  const elem = $('input[name="date"]');
+  const datepicker = new Datepicker(elem, {
+    // ...options
+  });
+  console.log(elem)
+})
