7cb7bc0b10c172d8d9ecfae9b4d2e7292353d1f1
Vic <syuan0212@gmail.com>
2022-08-21 17:34:53 +0800

    Update 2022-08-21T09:34:47.780Z

---
+++ assets/js/all.js
@@ -0,0 +1,13 @@
+"use strict";
+
+console.log('Hello!');
+$(function () {});
+var elem = document.getElementById('picker');
+
+if (elem) {
+  var datepicker = new Datepicker(elem, {// ...options
+  });
+}
+
+AOS.init();
+//# sourceMappingURL=all.js.map
